7f5a8f9c9322632b1830cfcde945a047b812cb95
Christos <34384856+ckpantelides@users.noreply.github.com>
2019-03-30 16:20:52 +0000

    Delete scraper.test.js

--- test/scraper.test.js
+++
@@ -1,8 +0,0 @@
-var scraper = require('../modules/scraper');
-var chai = require('chai');
-
-describe('webText', function () {
-  it('should be a string', function () {
-  chai.expect(scraper.webText).to.be.an('object');
-  });
-});
